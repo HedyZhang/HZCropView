0859844b62484552119f0f0d2684d094ae763c3e
zhanghaidi <zhanghadi@iyunxiao.com>
2018-04-27 09:55:26 +0800

    Initial Commit

---
+++ CropOverlay/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CropOverlay
+//
+//  Created by zhanghaidi on 2018/4/27.
+//  Copyright © 2018年 zhanghaidi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
